97e16ca813cde4413fefda73be312249d8ebab88
KhanhRomVN <khanhromvn@gmail.com>
2024-10-08 17:20:04 +0700

    update

---
+++ src/main/java/com/example/pleno_spring/PlenoSpringApplication.java
@@ -0,0 +1,12 @@
+package com.example.pleno_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlenoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlenoSpringApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/pleno_spring/PlenoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pleno_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlenoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
